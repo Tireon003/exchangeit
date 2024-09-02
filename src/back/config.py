93d90bc11274cd05fd371dc61fead2372eb50e67
Tireon003 <gunash29@mail.ru>
2024-09-02 03:00:39 +0300

    initial commit

---
+++ src/back/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # Postgres variables
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    # API variables
+    API_EXT_PORT: int
+
+    @property
+    def db_url(self):
+        return (f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}"
+                f":{self.POSTGRES_PORT}/{self.POSTGRES_DB}")
+
+    @property
+    def api_external_port(self):
+        return self.API_EXT_PORT
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
---
+++ src/front/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      ExchangeIT!
+    </>
+  )
+}
+
+export default App
