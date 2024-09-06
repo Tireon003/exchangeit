89c7fa0bae65a5c546db00a268f27aeb890a8b1c
Tireon003 <gunash29@mail.ru>
2024-09-06 21:10:45 +0300

    modified: changed allowed origins from all to waitlist

--- src/back/main.py
+++ src/back/main.py
@@ -1,14 +1,19 @@
 import uvicorn
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, HTTPException, status
 from starlette.middleware.cors import CORSMiddleware
 from typing import Annotated
 
 from orm import Crud
 
+allowed_origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,7 +22,15 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    response = list()
+    response.append(await Crud.search_ads_order_by_public_date())
+    response.append(await Crud.search_ads_by_item_give("some item"))
+    response.append(await Crud.search_ads_by_item_get("another item"))
+    response.append(await Crud.search_ads_give_get("yet another item", "other item"))
+    return {
+        "message": response,
+        "status": "200 OK",
+    }
 
 
 @app.post("/create_user")
@@ -27,7 +40,6 @@ async def create_user(user_data: Annotated[dict, Body()]):
         "message": "User created",
         "data": user_data
     }
-
 
 
 if __name__ == "__main__":
