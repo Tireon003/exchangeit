f03743d009b9039cfccddee45e0e840b972099ca
Tireon003 <gunash29@mail.ru>
2024-09-09 00:24:45 +0300

    new: class ContactORM to make cruds with contacts

---
+++ src/back/orm/contact_queries.py
@@ -0,0 +1,20 @@
+from core import database as db
+from models import AdTable
+from schemas import ContactCardFromDB
+
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
+
+class ContactORM:
+
+    @staticmethod
+    async def get_contact_from_ad(ad_id: int):
+        async with (db.create_async_session() as session):
+            get_contact_from_ad_query = select(AdTable).options(joinedload(AdTable.ad_contacts))
+            result = await session.scalars(get_contact_from_ad_query)
+            ad_orm = result.one_or_none()
+            if not ad_orm:
+                return
+            contact_model = ContactCardFromDB.model_validate(ad_orm.ad_contacts)
+            return contact_model
