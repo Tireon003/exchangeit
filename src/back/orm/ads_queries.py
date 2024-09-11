8de501299442fc08ffcad8afc2d735614eab1a00
Tireon003 <gunash29@mail.ru>
2024-09-11 21:07:38 +0300

    fix: changed type hints

--- src/back/orm/ads_queries.py
+++ src/back/orm/ads_queries.py
@@ -9,7 +9,7 @@ from sqlalchemy import select, and_
 class AdsORM:
 
     @staticmethod
-    async def search_ads_order_by_public_date() -> list[AdTable] | None:
+    async def search_ads_order_by_public_date() -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).order_by(AdTable.created_at)
             result = await session.scalars(query)
@@ -17,7 +17,7 @@ class AdsORM:
             return ads_by_date
 
     @staticmethod
-    async def search_ads_by_item_give(item_name: str) -> list[AdTable] | None:
+    async def search_ads_by_item_give(item_name: str) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).where(AdTable.item_give.like(f"%{item_name}%"))
             result = await session.scalars(query)
@@ -25,7 +25,7 @@ class AdsORM:
             return ads_by_item_give
 
     @staticmethod
-    async def search_ads_by_item_get(item_name: str) -> list[AdTable] | None:
+    async def search_ads_by_item_get(item_name: str) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).where(AdTable.item_get.like(f"%{item_name}%"))
             result = await session.scalars(query)
@@ -33,7 +33,7 @@ class AdsORM:
             return ads_by_item_get
 
     @staticmethod
-    async def search_ads_give_get(item_give: str, item_get: str) -> list[AdTable] | None:
+    async def search_ads_give_get(item_give: str, item_get: str) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).where(and_(AdTable.item_give.like(f"%{item_give}%"),
                                                AdTable.item_get.like(f"%{item_get}%")))
@@ -42,7 +42,7 @@ class AdsORM:
             return ads_give_get
 
     @staticmethod
-    async def select_user_ads(user_id: int) -> list[AdTable] | None:
+    async def select_user_ads(user_id: int) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).filter_by(id=user_id)
             result = await session.scalars(query)
@@ -50,7 +50,7 @@ class AdsORM:
             return user_ads
 
     @staticmethod
-    async def select_favorite_list(user_id: int) -> list[AdTable] | None:
+    async def select_favorite_list(user_id: int) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(FavoriteTable).filter_by(user_id=user_id)
             result = await session.scalars(query)
@@ -75,7 +75,7 @@ class AdsORM:
             await session.commit()
 
     @staticmethod
-    async def select_ad_by_id(ad_id: int):
+    async def select_ad_by_id(ad_id: int) -> list[AdFromDB] | None:
         async with db.create_async_session() as session:
             query = select(AdTable).filter_by(id=ad_id)
             result = await session.scalars(query)
