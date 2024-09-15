ed3aba1091a8aaa2488c77c75f7c40c459ad6b2b
Tireon003 <gunash29@mail.ru>
2024-09-15 19:07:15 +0300

    refactoring: now getting db session in methods args

--- src/back/orm/contact_queries.py
+++ src/back/orm/contact_queries.py
@@ -1,31 +1,29 @@
-from core import database as db
 from models import AdTable, ContactTable
 from schemas import ContactCardFromDB
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 
 class ContactORM:
 
     @staticmethod
-    async def get_contact_from_ad(ad_id: int):
-        async with (db.create_async_session() as session):
-            get_contact_from_ad_query = select(AdTable). \
-                                        options(joinedload(AdTable.ad_contacts)). \
-                                        filter_by(id=ad_id)
-            result = await session.scalars(get_contact_from_ad_query)
-            ad_orm = result.one_or_none()
-            if not ad_orm:
-                return
-            contact_model = ContactCardFromDB.model_validate(ad_orm.ad_contacts)
-            return contact_model
+    async def get_contact_from_ad(ad_id: int, session: AsyncSession) -> ContactCardFromDB:
+        get_contact_from_ad_query = select(AdTable). \
+                                    options(joinedload(AdTable.ad_contacts)). \
+                                    filter_by(id=ad_id)
+        result = await session.scalars(get_contact_from_ad_query)
+        ad_item = result.one_or_none()
+        if not ad_item:
+            return
+        contact_model = ContactCardFromDB.model_validate(ad_item.ad_contacts)
+        return contact_model
 
     @staticmethod
-    async def get_contact_id_by_user_id(user_id: int) -> int:
-        async with db.create_async_session() as session:
-            query = select(ContactTable).filter_by(by_user=user_id)
-            result = await session.scalars(query)
-            contacts = result.one_or_none()
-            return contacts.id if contacts else None
+    async def get_contact_id_by_user_id(user_id: int, session: AsyncSession) -> int | None:
+        query = select(ContactTable).filter_by(by_user=user_id)
+        result = await session.scalars(query)
+        contacts = result.one_or_none()
+        return contacts.id if contacts else None
 
--- src/back/orm/user_queries.py
+++ src/back/orm/user_queries.py
@@ -1,60 +1,55 @@
 from models import UserTable, ContactTable, AdTable
 from schemas import UserCreate, UserUpdate, ContactCardCreate, UserFromDB
-from core import database as db
 from fastapi_cache.decorator import cache
 from sqlalchemy import delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserORM:
 
     @staticmethod
-    async def create_user(user_obj: UserCreate) -> UserFromDB:
-        async with db.create_async_session() as session:
-            new_user = UserTable(**user_obj.model_dump())
-            session.add(new_user)
-            await session.flush()
-            await session.refresh(new_user)
-            contacts_model = ContactCardCreate.model_validate(
-                {
-                    "by_user": new_user.id,
-                }
-            )
-            contact_card = ContactTable(**contacts_model.model_dump())
-            session.add(contact_card)
-            await session.commit()
-            created_user_model = UserFromDB.model_validate(new_user)
-            return created_user_model
+    async def create_user(user_obj: UserCreate, session: AsyncSession) -> UserFromDB:
+        new_user = UserTable(**user_obj.model_dump())
+        session.add(new_user)
+        await session.flush()
+        await session.refresh(new_user)
+        contacts_model = ContactCardCreate.model_validate(
+            {
+                "by_user": new_user.id,
+            }
+        )
+        contact_card = ContactTable(**contacts_model.model_dump())
+        session.add(contact_card)
+        await session.commit()
+        created_user_model = UserFromDB.model_validate(new_user)
+        return created_user_model
 
     @staticmethod
     @cache(expire=60)
-    async def select_user_by_id(user_id: int) -> UserFromDB:
-        async with db.create_async_session() as session:
-            query = select(UserTable).filter_by(id=user_id)
-            result = await session.scalars(query)
-            user = result.one_or_none()
-            if not user:
-                return
-            user_model = UserFromDB.model_validate(user)
-            return user_model
+    async def select_user_by_id(user_id: int, session: AsyncSession) -> UserFromDB:
+        query = select(UserTable).filter_by(id=user_id)
+        result = await session.scalars(query)
+        user = result.one_or_none()
+        if not user:
+            return
+        user_model = UserFromDB.model_validate(user)
+        return user_model
 
     @staticmethod
-    async def select_user_by_username(username: str):
-        async with db.create_async_session() as session:
-            query = select(UserTable).filter_by(username=username)
-            result = await session.scalars(query)
-            user = result.one_or_none()
-            return user
+    async def select_user_by_username(username: str, session: AsyncSession):
+        query = select(UserTable).filter_by(username=username)
+        result = await session.scalars(query)
+        user = result.one_or_none()
+        return user
 
     @staticmethod
-    async def update_user_password(updated_user_obj: UserUpdate):
-        async with db.create_async_session() as session:
-            user = await session.get(UserTable, updated_user_obj.id)
-            user = UserTable(**updated_user_obj.model_dump())
-            await session.commit()
+    async def update_user_password(updated_user_obj: UserUpdate, session: AsyncSession):
+        user = await session.get(UserTable, updated_user_obj.id)
+        user = UserTable(**updated_user_obj.model_dump())
+        await session.commit()
 
     @staticmethod
-    async def delete_user(user_id: int):
-        async with db.create_async_session() as session:
-            query = delete(UserTable).filter_by(id=user_id)
-            await session.execute(query)
-            await session.commit()
+    async def delete_user(user_id: int, session: AsyncSession):
+        query = delete(UserTable).filter_by(id=user_id)
+        await session.execute(query)
+        await session.commit()
