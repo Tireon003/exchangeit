8e10a8eb9775eebce062c7ba99e0ea32de955361
Tireon003 <gunash29@mail.ru>
2024-09-15 19:13:26 +0300

    fetch ads and return search panel and ads list

--- src/front/src/App.jsx
+++ src/front/src/App.jsx
@@ -1,12 +1,43 @@
+import { useState, useEffect, createContext } from 'react';
+import AdList from './modules/AdList';
+import SearchPanel from './modules/SearchPanel';
+import axios from 'axios';
 
+const SearchContext = createContext();
 
-function App() {
+const App = () => {
+
+  const [searchResult, setSearchResult] = useState(["", ""]);
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    fetchAds();
+  }, [searchResult]);
+
+  async function fetchAds() {
+    try {
+      const response = await axios.post(
+          'http://localhost:8008/api/ads/',
+          {
+              itemGet: searchResult[1],
+              itemGive: searchResult[0],
+          }
+      );
+      setAds(response.data);
+    } catch (error) {
+      console.error('Ошибка при получении объявлений:', error);
+    }
+  }
 
   return (
-    <>
-      ExchangeIT!
-    </>
-  )
-}
+      <SearchContext.Provider value={[searchResult, setSearchResult]}>
+          <div className="mx-auto px-24">
+              <SearchPanel />
+              <AdList ads={ads}/>
+          </div>
+      </SearchContext.Provider>
+  );
+};
 
-export default App
+export default App;
+export { SearchContext };
