ca00d44a8710094afa404e291a32d869f566da74
Tireon003 <gunash29@mail.ru>
2024-09-19 00:38:59 +0300

    refactored imports

--- src/back/routes/user.py
+++ src/back/routes/user.py
@@ -4,16 +4,15 @@ from fastapi import (
     Path,
     status,
     Body,
-
+    HTTPException,
 )
 
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from dependencies import get_async_session
-from schemas import UserFromDB, AdCreate, CreatedResponse
+from dependencies import get_async_session, validate_token
+from schemas import UserFromDB, AdCreate, CreatedResponse, AccessTokenPayload
 from orm import UserORM, AdsORM
-from exceptions import UserNotFoundException
 
 router = APIRouter(
     prefix="/api/users",
@@ -36,16 +35,23 @@ async def get_user(
         raise UserNotFoundException(user_id)
 
 
-@router.put("/{user_id}/ads/new", response_model=CreatedResponse)
+@router.post("/{user_id}/ads/new", response_model=CreatedResponse)
 async def create_ad(
         ad_data: Annotated[AdCreate, Body()],
         user_id: Annotated[int, Path()],
+        token_payload: Annotated[AccessTokenPayload, Depends(validate_token)],
         db_session: AsyncSession = Depends(get_async_session)
 ):
-    await AdsORM.insert_ad(
-        ad=ad_data,
-        user_id=user_id,
-        session=db_session
-    )
-    return CreatedResponse()
 
+    if user_id != token_payload.usr:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User id in path does not match token user id"
+        )
+    else:
+        await AdsORM.insert_ad(
+            ad=ad_data,
+            user_id=user_id,
+            session=db_session
+        )
+        return CreatedResponse()
