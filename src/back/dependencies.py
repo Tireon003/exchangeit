2f5c8b0d659e749fb3e81661316d5f3830ca8738
Tireon003 <gunash29@mail.ru>
2024-09-19 00:26:18 +0300

    implemented oauth2 login with bearer jwt token

--- src/back/dependencies.py
+++ src/back/dependencies.py
@@ -1,9 +1,14 @@
 from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import database as db
+from services import JwtService
+from schemas import AccessTokenPayload, UserCreds
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
@@ -15,3 +20,13 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
         raise
     finally:
         await session.close()
+
+
+def validate_token(access_token: Annotated[str, Depends(oauth2_scheme)]):
+    payload = JwtService.verify_token(access_token)
+    payload_model = AccessTokenPayload(**payload)
+    return payload_model
+
+
+def get_login_form(credentials: Annotated[UserCreds, Depends(OAuth2PasswordRequestForm)]):
+    return credentials
--- src/back/routes/auth.py
+++ src/back/routes/auth.py
@@ -1,18 +1,22 @@
 from typing import Annotated
+from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi import (
     APIRouter,
     Body,
     status,
-    Depends
+    Depends,
+    HTTPException,
+    Response,
 )
 
-from schemas import UserCreate, CreatedResponse, UserExistsResponse
+from schemas import UserCreate, CreatedResponse, UserExistsResponse, AccessTokenPayload, UserCreds, TokenType
 from services import HashingService as HS
+from services import JwtService
 from orm import UserORM
-from exceptions import UserAlreadyExists
-from dependencies import get_async_session
+from exceptions import UserAlreadyExists, InvalidTokenException, ExpiredTokenException, UserNotFoundException
+from dependencies import get_async_session, validate_token, get_login_form
 
 
 router = APIRouter(
@@ -34,3 +38,38 @@ async def sign_up(
         await UserORM.create_user(user_creds, db_session)
         return CreatedResponse()
 
+
+@router.post("/login")
+async def login_user(
+    response: Response,
+    login_creds: Annotated[UserCreds, Depends(get_login_form)],
+    db_session: AsyncSession = Depends(get_async_session)
+):
+    user_from_db = await UserORM.select_user_by_username(login_creds.username, db_session)
+    print(user_from_db.model_dump())
+    if not user_from_db:
+        raise UserNotFoundException(login_creds.username)
+    if not HS.verify(login_creds.password, user_from_db.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Wrong password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    payload = {"usr": user_from_db.id}
+    user_access_token = JwtService.create_token(
+        payload=payload,
+        token_type=TokenType.access,
+    )
+    response.set_cookie(
+        key="access_token",
+        value=user_access_token,
+        httponly=True,
+        secure=True,
+    )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "access_token": user_access_token,
+            "token_type": "Bearer",
+        }
+    )
