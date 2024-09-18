f49b31d77a0cd92e488581cb0b3954147cd6bcb1
Tireon003 <gunash29@mail.ru>
2024-09-19 00:23:21 +0300

    new token deps

--- src/back/schemas/__init__.py
+++ src/back/schemas/__init__.py
@@ -1,10 +1,20 @@
 from .ad import AdFromDB, AdCreate, AdUpdate, AdDBRelCategory, AdDBRelBelongsto, AdDBRelInFavorites
 from .category import CategoryFromDB, CategoryCreate, CategoryDBRelAds
 from .contacts import ContactCardFromDB, ContactCardCreate, ContactCardDBRelUser
-from .user import UserFromDB, UserCreate, UserUpdate, UserLogin, UserDBRelAds, UserDBRelContacts, UserDBRelFavAds
+from .user import (
+    UserFromDB,
+    UserCreate,
+    UserUpdate,
+    UserLogin,
+    UserDBRelAds,
+    UserDBRelContacts,
+    UserDBRelFavAds,
+    UserCreds,
+)
 from .ads_contacts import AdsContactsFromDB
 from .search import SearchData
 from .responses import BaseResponse, CreatedResponse, UserExistsResponse, UserDoesNotExistsResponse
+from .token_schemas import TokenType, AccessTokenPayload
 
 __all__ = (
     'AdFromDB',
@@ -26,10 +36,13 @@ __all__ = (
     'UserDBRelContacts',
     'UserDBRelFavAds',
     'UserUpdate',
+    'UserCreds',
     'AdsContactsFromDB',
     'SearchData',
     'BaseResponse',
     'CreatedResponse',
     'UserExistsResponse',
-    'UserDoesNotExistsResponse'
+    'UserDoesNotExistsResponse',
+    'AccessTokenPayload',
+    'TokenType',
 )
