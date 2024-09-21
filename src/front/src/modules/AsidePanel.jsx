042318d8087840a20e9ac4f849ce9494abfd7302
Tireon003 <gunash29@mail.ru>
2024-09-21 22:49:27 +0300

    added logic to login, logout, signup

--- src/front/src/modules/AsidePanel.jsx
+++ src/front/src/modules/AsidePanel.jsx
@@ -1,47 +1,66 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import {SignupModal} from '../components/SignupModal'
+import {LoginModal} from '../components/LoginModal'
 
 
 const AsidePanel = () => {
 
     const [userData, setUserData] = useState(null);
-    const [isModal, setIsModal] = useState(false);
+    const [isSignupModal, setIsSignupModal] = useState(false);
+    const [isLoginModal, setIsLoginModal] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const closeModal = () => {
-        setIsModal(false);
+    const setLoggedParam = () => {
+        setIsLoggedIn(true);
     }
 
-    const openModal = () => {
-        setIsModal(true);
+    const closeSignupModal = () => {
+        setIsSignupModal(false);
+    }
+
+    const openSignupModal = () => {
+        setIsSignupModal(true);
+    }
+
+    const closeLoginModal = () => {
+        setIsLoginModal(false);
+    }
+
+    const openLoginModal = () => {
+        setIsLoginModal(true);
     }
 
     const getUserData = async () => {
         try {
             const token = Cookies.get('access_token');
-            if (!token) {
-                throw new Error('Access token not found in cookies');
-            }
-
-            const response = await axios.get(
-                'http://localhost:8008/api/users/user',
-                {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
+            if (token) {
+                const response = await axios.get(
+                    'http://localhost:8008/api/users/user',
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
                     }
-                }
-            );
-
-            setUserData(response.data);
+                );
+                setUserData(response.data);
+            }
         } catch (error) {
             console.error("Ошибка при получении данных пользователя:", error);
+            await logOutUser();
         }
     };
 
+    const logOutUser = async () => {
+        Cookies.remove('access_token')
+        setUserData(null);
+        setIsLoggedIn(false);
+    }
+
     useEffect(() => {
         getUserData();
-    }, []);
+    }, [isLoggedIn]);
 
     return (
         <div className="flex flex-col items-center w-64 min-w-64 shadow-lg h-full p-4 box-border">
@@ -62,6 +81,12 @@ const AsidePanel = () => {
                   <div className="p-2 mb-2 rounded-md hover:bg-gray-200 cursor-pointer">
                     Profile
                   </div>
+                  <div
+                    className="p-2 mb-2 rounded-md hover:bg-gray-200 cursor-pointer"
+                    onClick={logOutUser}
+                  >
+                    Log out
+                  </div>
                 </>
 
             ) : (
@@ -69,17 +94,21 @@ const AsidePanel = () => {
                     <div>You are not logged in. Sign up or log in to open all features</div>
                     <button
                         className="mt-2 w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300"
-                        onClick={openModal}
+                        onClick={openSignupModal}
                     >
                       Sign Up
                     </button>
 
-                    <button className="mt-2 w-full border border-blue-500 bg-transparent px-4 py-2 rounded-md text-blue-500 hover:bg-blue-100 transition-all duration-300">
+                    <button
+                        className="mt-2 w-full border border-blue-500 bg-transparent px-4 py-2 rounded-md text-blue-500 hover:bg-blue-100 transition-all duration-300"
+                        onClick={openLoginModal}
+                    >
                       Log In
                     </button>
                 </>
             )}
-            <SignupModal isOpen={isModal} onClose={closeModal}/>
+            <SignupModal isOpen={isSignupModal} onClose={closeSignupModal}/>
+            <LoginModal isOpen={isLoginModal} onClose={closeLoginModal} setLogged={setLoggedParam}/>
         </div>
     );
 };
